8d68c62244aa47754a3bf89661a36469b0b71b21
nekowawolf <irukainu20@gmail.com>
2025-08-21 02:31:49 +0700

    feat: add AirdropEnded, AirdropFree, and AirdropPaid pages

---
+++ src/app/dashboard/airdrop-free/page.tsx
@@ -0,0 +1,22 @@
+export default function AirdropFreePage() {
+  return (
+    <div className="space-y-6">
+      <div className="text-center sm:text-left">
+        <h2 className="text-lg sm:text-2xl font-semibold text-primary">
+          Airdrop Free
+        </h2>
+        <p className="text-xs sm:text-sm text-secondary">
+          List of free airdrop campaigns
+        </p>
+      </div>
+
+    <section className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4">
+      
+    </section>
+
+    <section className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+      
+    </section>
+  </div>
+  )
+}
---
+++ src/app/dashboard/airdrop-paid/page.tsx
@@ -0,0 +1,22 @@
+export default function AirdropPaidPage() {
+  return (
+    <div className="space-y-6">
+      <div className="text-center sm:text-left">
+        <h2 className="text-lg sm:text-2xl font-semibold text-primary">
+          Airdrop Paid
+        </h2>
+        <p className="text-xs sm:text-sm text-secondary">
+          List of paid airdrop campaigns
+        </p>
+      </div>
+
+    <section className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4">
+      
+    </section>
+
+    <section className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+      
+    </section>
+  </div>
+  )
+}

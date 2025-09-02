584b30e2671fde148f571dc7f6191cd4c8b2eda0
nekowawolf <irukainu20@gmail.com>
2025-09-03 03:09:16 +0700

    feat: add metadata for Add Airdrop page

--- src/app/add-airdrop/page.tsx
+++ src/app/add-airdrop/page.tsx
@@ -1,4 +1,7 @@
 import AddAirdropForm from '@/components/AddAirdropForm'
+import { dashboardMetadata } from '@/constants/metadataTemplates'
+
+export const metadata = dashboardMetadata('Add Airdrop', 'Add new airdrop campaign')
 
 export default function Page() {
   return (

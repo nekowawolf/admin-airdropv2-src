ed777a7177fe51608be813eec84c23a0ae29baa6
nekowawolf <irukainu20@gmail.com>
2025-09-19 01:38:56 +0700

    feat: add toast notification to auth guard

--- src/hooks/useAuthGuard.ts
+++ src/hooks/useAuthGuard.ts
@@ -3,12 +3,15 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import Cookies from 'js-cookie'
+import { toast } from 'sonner'
 
 export function useAuthGuard() {
   const router = useRouter()
+  
   useEffect(() => {
     const token = Cookies.get('token')
     if (!token) {
+      toast.error('Login first to continue')
       router.push('/login')
     }
   }, [router])

0065febac6c53e131ad860173c1cad8e3d0d3ab2
nekowawolf <irukainu20@gmail.com>
2025-10-01 00:41:15 +0700

    refactor: consolidate airdrop interfaces to extend AirdropBase

--- src/types/airdrop.d.ts
+++ src/types/airdrop.d.ts
@@ -1,4 +1,4 @@
-export interface AirdropFormData {
+export interface AirdropBase {
   name: string
   task: string
   link: string
@@ -8,56 +8,16 @@ export interface AirdropFormData {
   funds: string
   supply: string
   market_cap: string
-  price: string
   vesting: string
-  usd_income: string
   claim: string
-}
-
-export interface AirdropRequest {
-  name: string
-  task: string
-  link: string
-  level: string
-  status: string
-  backed: string
-  funds: string
-  supply: string
-  market_cap: string
-  vesting: string
-  link_claim: string
   price: number
   usd_income: number
 }
 
-export interface AirdropFreeRequest {
-  name: string;
-  task: string;
-  link: string;
-  level: string;
-  status: string;
-  backed: string;
-  funds: string;
-  supply: string;
-  market_cap: string;
-  vesting: string;
-  link_claim: string;
-  price: number;
-  usd_income: number;
-}
+export interface AirdropFormData extends AirdropBase {}
 
-export interface AirdropPaidRequest {
-  name: string;
-  task: string;
-  link: string;
-  level: string;
-  status: string;
-  backed: string;
-  funds: string;
-  supply: string;
-  market_cap: string;
-  vesting: string;
-  link_claim: string;
-  price: number;
-  usd_income: number;
-}
+export interface AirdropRequest extends AirdropBase {}
+
+export interface AirdropFreeRequest extends AirdropBase {}
+
+export interface AirdropPaidRequest extends AirdropBase {}
